cf4e4aeb9fc4a2b28b1f162adeada1d0286c1cad
Max Chu <maxchu2021@gmail.com>
2026-06-27 19:42:19 +0800

    feat(dark-mode): 新增上方 Dark Mode 切換開關並套用燈光 preset

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import LoadingPage from "./Experience/component/dom/LoadingPage";
+import DarkModeToggle from "./Experience/component/dom/DarkModeToggle";
 import Experience from "./Experience/Experience";
 
 function App() {
     return (
         <>
             <LoadingPage />
+            <DarkModeToggle />
             <Experience />
         </>
     );
